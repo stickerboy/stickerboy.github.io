a69c074674641863a457c966c52e609d575c7c4c
Kenny <stickerboy@users.noreply.github.com>
2022-06-26 18:21:59 +0100

    Kenny 1.0
    The beginning...

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/*!
+* Start Bootstrap - Landing Page v6.0.5 (https://startbootstrap.com/theme/landing-page)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
+*/
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+const resume = document.getElementById("resume");
+const email = "xc.ynnek@yeh";
+const subject = "Request for CV / Resume";
+const emailRev = email.split("").reverse().join("");
+resume.setAttribute("href", "mailto:" + emailRev + "?subject=" + subject);
