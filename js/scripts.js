41311809d7de1c6f1711eab5685709648db69fe9
Kenny <stickerboy@users.noreply.github.com>
2022-10-19 00:29:16 +0100

    Null check instead of length

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
-if(document.getElementById("resume").length > 0) {
+if(document.getElementById("resume") !== null) {
     const resume = document.getElementById("resume");
     const email = "xc.ynnek@yeh";
     const subject = "Request for CV / Resume";
