37fa7328c3059e89ec1af703a475a21fb1ace1aa
Kenny <stickerboy@users.noreply.github.com>
2024-01-08 00:57:31 +0000

    11ty static site build

---
+++ _site/js/scripts.js
@@ -0,0 +1,32 @@
+/*!
+* 
+* kenny.cx v1.1.0 (https://www.kenny.cx)
+* Copyright 2024 @stickerboy / Kenny Cameron
+*/
+function b64EncodeUnicode(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode('0x' + p1);
+    }));
+}
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+if(document.getElementById("resume") !== null) {
+    const resume = document.getElementById("resume");
+    const email = "xc.ynnek@yeh";
+    const subject = "Request for CV / Resume";
+    const emailRev = email.split("").reverse().join("");
+    resume.setAttribute("href", "mailto:" + emailRev + "?subject=" + subject);
+}
+
+const curious = document.getElementById("curious");
+const curiouser = document.getElementById("curiouser");
+curious.addEventListener("click", function() {
+    this.classList.toggle("me-4");
+    curiouser.classList.toggle("visually-hidden");
+});
+
+curiouser.addEventListener("click", function() {
+    window.location = "../==wc19WayV3Y";
+});
---
+++ _site/js/wc19WayV3Y.js
@@ -0,0 +1,25 @@
+let codeInput = document.getElementById("code");
+function codeDrop() {
+    codeInput.value = codeInput.value + this.innerHTML;
+}
+const elements = document.getElementsByClassName("btn--code"); 
+Array.from(elements, c => c.addEventListener('click', codeDrop));
+
+const verify = document.getElementById("verify");
+verify.addEventListener("click", function() {
+    console.log(codeInput.value);
+    if(b64EncodeUnicode(codeInput.value) === "R3Y3M2o2alVWcQ==") {
+        verify.classList.remove("btn-outline-secondary", "btn-danger");
+        verify.classList.add("btn-success");
+        codeInput.classList.remove("is-invalid");
+        codeInput.classList.add("is-valid");
+        setTimeout(() => {
+            window.location = `../${atob("c21pcmtsZQ==")}`;
+        }, 2000);
+        return;
+    }
+    verify.classList.remove("btn-outline-secondary", "btn-success");
+    verify.classList.add("btn-danger");
+    codeInput.classList.remove("is-valid");
+    codeInput.classList.add("is-invalid");
+});
