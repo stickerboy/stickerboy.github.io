e263e2252b896c7985ae7078d320b5cfcad90386
Kenny <stickerboy@users.noreply.github.com>
2026-04-07 12:39:21 +0100

    WIP

---
+++ eleventy.config.js
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+
+import cssnano from 'cssnano';
+import postcss from 'postcss';
+import tailwindcss from '@tailwindcss/postcss';
+
+export default function (eleventyConfig) {
+  //compile tailwind before eleventy processes the files
+  eleventyConfig.on('eleventy.before', async () => {
+    const tailwindInputPath = path.resolve('./src/assets/styles/index.css');
+
+    const tailwindOutputPath = './dist/assets/styles/index.css';
+
+    const cssContent = fs.readFileSync(tailwindInputPath, 'utf8');
+
+    const outputDir = path.dirname(tailwindOutputPath);
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    const result = await processor.process(cssContent, {
+      from: tailwindInputPath,
+      to: tailwindOutputPath,
+    });
+
+    fs.writeFileSync(tailwindOutputPath, result.css);
+  });
+
+  const processor = postcss([
+    //compile tailwind
+    tailwindcss(),
+
+    //minify tailwind css
+    cssnano({
+      preset: 'default',
+    }),
+  ]);
+
+  return {
+    dir: { input: 'src', output: 'dist' },
+  };
+}
