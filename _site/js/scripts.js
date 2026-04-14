83169288a97a26e79d815488502d299de67676c8
Kenny C <stickerboy@users.noreply.github.com>
2026-04-14 11:00:32 +0100

    Remove comment

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,9 +1,6 @@
 /*!
 * scripts.js
 */
-
-// ...existing code...
-
 function b64EncodeUnicode(str) {
     return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
         return String.fromCharCode('0x' + p1);
